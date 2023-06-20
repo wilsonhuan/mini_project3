32e4a3a2996820189b83bae705d7e70300d3545a
wilsonhuan <hi4u2hs999@gmail.com>
2023-06-20 22:41:46 +0800

    add submission

---
+++ src/policy/submission.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "../state/state.hpp"
+
+
+/**
+ * @brief Policy class for random policy, 
+ * your policy class should have get_move method
+ */
+class abpruning{
+public:
+    static Move get_move(State *state, int depth);
+    static int find_value(State* state, int depth, int alpha, int beta, bool max);
+};
